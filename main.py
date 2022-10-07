dc1c185e1b63cb4d29255183c0fef30d8140d3d4
brunofavs <bruno.favs@ua.pt>
2022-10-07 17:39:20 +0100

    Completed the skeleton

--- main.py
+++ main.py
@@ -17,9 +17,22 @@ colorama.init(autoreset=True)
 
 #Global Variables
 
-input_tuple=namedtuple('input_tuple',['l_s','l_t','t'])
+input_tuple=namedtuple('input_tuple',['l_s','l_t','t']) # letter_shown,letter_typed,time
 
 #Functions
+
+def modoCount(threshold):
+    print('Modo nº palavras')
+
+def modoTimed(threshold):
+    print('Modo timed')
+    # TODO Yet to implement
+    # return timed_inputs
+
+def buildDict(inputs):
+    pass
+
+    # return {accuracy:blablalba}
 
 
 #Main
@@ -31,9 +44,20 @@ def main():
     parser.add_argument('-mv','--max_value',type=int,required=True,help='Number of seconds/letters of the test') 
     args = parser.parse_args()
 
-   
+    inputs = [] #* This will be the list of namedTuples that the function buildDict will use to build the statistics dictionary
+    my_dict = {}
 
+    if args.utm == True:
+        inputs = modoTimed(args.max_value)
+    else:
+        inputs = modoCount(args.max_value)
+        
+    #At this point in the programm there should already be the list of namedTuples on which the buildDict function will work with
+    my_dict = buildDict(inputs)
+    print(my_dict) #! --> prints 'none' because buildDict returns void
 
+    
+    
 
 
 if __name__=='__main__':
