76b3cba3944dbba9694a555fad920dcb595952fb
fabio-rcs <fabiorsousa81@ua.pt>
2022-10-07 20:00:19 +0100

    Some changes

--- main.py
+++ main.py
@@ -28,19 +28,64 @@ def modoCount(threshold):
     print('The test will begin shortly, ending after pressing ' + str(threshold) + ' letters.')
     print('Press any key to begin the test!')
     getch.getch() # See line 13
+    #_ = readchar.readchar()
     
     # After pressing a key, theres a 3 second countdown before the test starts for the user to prepare.
     for i in range(1,4):
-        print('The test will start in '+ i +' seconds.')
+        print('The test will start in '+ str(4-i) +' seconds.')
         time.sleep(1)
     
-    for i in range(1,)
+    #for i in range(1,)
     
-   
 
 
+def modoTimed(threshold):
+    print('The test will begin shortly, ending after ' + str(threshold) + ' seconds.')
+    print('Press any key to begin the test')
+    getch.getch() # See line 13 
+
+    # After pressing a key, there's a 3 second countdown before the test starts for the user to prepare.
+    for i in range(1,4):
+        print('The test will start in '+ str(4-i) +' seconds.')
+        time.sleep(1)
 
 
+    # TODO Yet to implement
+    # return timed_inputs
+
+def buildDict(inputs):
+    pass
+
+    # return {accuracy:blablalba}
+
+
+#Main
+
+def main():
+    
+    parser = argparse.ArgumentParser(description='Script for testing typing speed and accuracy') 
+    parser.add_argument('-utm','--use_time_mode',action='store_true',default = False ,help='Use timed mode : tests up to max_value seconds.\n Otherwise tests up to max_value letters')
+    parser.add_argument('-mv','--max_value',type=int,required=True,help='Number of seconds/letters of the test') 
+    args = parser.parse_args()
+
+    inputs = [] #* This will be the list of namedTuples that the function buildDict will use to build the statistics dictionary
+    my_dict = {}
+
+    if args.utm == True:
+        inputs = modoTimed(args.max_value)
+    else:
+        inputs = modoCount(args.max_value)
+        
+    #At this point in the programm there should already be the list of namedTuples on which the buildDict function will work with
+    my_dict = buildDict(inputs)
+    print(my_dict) #! --> prints 'none' because buildDict returns void
+
+
+    
+
+
+if __name__=='__main__':
+    main()
 
 
 
